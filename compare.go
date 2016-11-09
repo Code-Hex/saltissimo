c204124c1364a57a012e89966a8032e3e6790f67
K <x00.x7f@gmail.com>
2016-11-10 00:19:09 +0900

    Added comment

--- compare.go
+++ compare.go
@@ -5,6 +5,17 @@ import (
 	"hash"
 )
 
+// Suppose you have saved like this data.
+// +--------------------------+
+// | HMAC_hexstr | secret_key |
+// +--------------------------+
+// | 5f54e622... | 951ff34... |
+// +--------------------------+
+// Then, you are passed any string. Assume that you are given a "password" here.
+// You can use compare function looks like this:
+//  saltissimo.CompareHexHash(sha256.New, "password", HMAC_hexstr, secret_key)
+
+// CompareHexHash to compare passed string and HMAC as hex string.
 func CompareHexHash(hash func() hash.Hash, str, hexStr, key string) (bool, error) {
 	kb, err := hex.DecodeString(key)
 	if err != nil {
@@ -13,6 +24,7 @@ func CompareHexHash(hash func() hash.Hash, str, hexStr, key string) (bool, error
 	return HmacToHex(hash, str, kb) == hexStr, err
 }
 
+// CompareB64Hash to compare passed string and HMAC as base64 string.
 func CompareB64Hash(hash func() hash.Hash, str, b64Str, key string) (bool, error) {
 	kb, err := hex.DecodeString(key)
 	if err != nil {
