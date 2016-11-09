e3294c570f4d5c654f01e297c838d3343478859d
K <x00.x7f@gmail.com>
2016-11-10 00:33:56 +0900

    Modified comment for GoDoc

--- compare.go
+++ compare.go
@@ -1,19 +1,21 @@
+// Package saltissimo was developed to easily compare hash of salt password.
+// Suppose you have saved like this data.
+//
+// +--------------------------+
+// | HMAC_hexstr | secret_key |
+// +--------------------------+
+// | 5f54e622... | 951ff34... |
+// +--------------------------+
+//
+// Then, you are passed any string. Assume that you are given a "password" here.
+// You can use compare function looks like this:
+//  saltissimo.CompareHexHash(sha256.New, "password", HMAC_hexstr, secret_key)
 package saltissimo
 
 import (
 	"encoding/hex"
 	"hash"
 )
-
-// Suppose you have saved like this data.
-// +--------------------------+
-// | HMAC_hexstr | secret_key |
-// +--------------------------+
-// | 5f54e622... | 951ff34... |
-// +--------------------------+
-// Then, you are passed any string. Assume that you are given a "password" here.
-// You can use compare function looks like this:
-//  saltissimo.CompareHexHash(sha256.New, "password", HMAC_hexstr, secret_key)
 
 // CompareHexHash to compare passed string and HMAC as hex string.
 func CompareHexHash(hash func() hash.Hash, str, hexStr, key string) (bool, error) {
